4a98add813dddc3ff28da3cf42f9201aa19ba454
jessielw <jessielw4049@gmail.com>
2026-07-21 17:33:50 -0400

    feat: add some basic navigation via keyboard to go to next video in Jellyfin

---
+++ resources/mpv/jellyfin_dc.lua
@@ -0,0 +1,15 @@
+local mp = require 'mp'
+
+local function send_control(action, label)
+    mp.osd_message(label, 1)
+    mp.commandv('script-message', 'jellyfin-dc-control', action)
+end
+
+mp.add_key_binding('>', 'next', function()
+    send_control('next', 'Next item')
+end)
+
+mp.add_key_binding('<', 'previous', function()
+    send_control('previous', 'Previous item')
+end)
+
